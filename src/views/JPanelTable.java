eefa3226f1f0258bf6d6a691678457b9c7e2a1d5
jairoalzate98 <jairo.alzate@uptc.edu.co>
2018-06-13 11:40:24 -0500

    Se agrego una matriz en la cual se van a crear las relaciones entre nodos

--- src/views/JPanelTable.java
+++ src/views/JPanelTable.java
@@ -2,8 +2,12 @@ package views;
 
 import java.awt.Color;
 import java.awt.GridLayout;
+import java.awt.Label;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 import controllers.Controller;
 
@@ -14,9 +18,26 @@ public class JPanelTable extends JPanel{
 	public JPanelTable(Controller controller, int number) {
 		setBackground(Color.WHITE);
 		setLayout(new GridLayout(number + 1, number + 1));
-		for (int i = 0; i < number; i++) {
-			for (int j = 0; j < number; j++) {
-				
+		String[][] tables = new String[number + 1][number + 1];
+		int num = 1;
+		for (int i = 0; i <= number; i++) {
+			for (int j = 0; j <= number; j++) {
+				if (i == 0 && j != 0) {
+					tables[i][j] = String.valueOf(j);
+					add(new JLabel(j+"" , SwingConstants.CENTER));
+				}else {
+					if (i == 0 && j == 0) {
+						add(new JLabel(""));
+					}else {
+						if (j == 0 && i != 0) {
+							tables[i][j] = String.valueOf(num);
+							add(new JLabel(num+"", SwingConstants.CENTER));
+							num++;
+						}else {
+							add(new JTextField());
+						}
+					}
+				}
 			}
 		}
 	}
